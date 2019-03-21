24a241ac3e655fcbe8049f65d32e336c90936067
mercelessmonk <mercelessmonk@gmail.com>
2019-03-22 00:01:19 +0530

    event propogation is handled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import * as $ from 'jquery';
+declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    $('.jq').mouseover(function () {
+      $(this).css('color', 'red');
+    });
+    $('.jq').mouseout(function () {
+      if ($(this).is(":focus")) {
+
+        $(this).css('color', 'green');
+        event.stopImmediatePropagation();
+      } else {
+        $(this).css('color', 'blue');
+      }
+    });
+  }
+
 }
